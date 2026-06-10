d12055406a631ee4b81744f110fbe56471d31422
Ashish Bedare <ashishbedare9@gmail.com>
2026-06-10 21:00:40 +0530

    check driver connection

---
+++ src/SQL_pr.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+
+//import packages
+// //load and register the Driver
+// Create Connection
+// create a statement
+// execute the query
+// process the result
+// close
+public class SQL_pr {
+    public static void main(String[] args) throws Exception{
+//        String url="";
+//        String name="Root";
+//        String pass="";
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        Connection con=DriverManager.getConnection(url,name,pass);
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Loaded Sucessfully");
+
+        }catch(Exception e){
+
+        e.printStackTrace();
+
+        }
+    }
+
+}
